6708adb69d3619329b39499aa50b9991a1cf51c8
Markos <markos@joomlashack.com>
2014-04-23 18:10:30 -0600

    TPLUNLIMITED-35 #comment adding style to featured blog

---
+++ code/html/com_content/featured/default_item.php
@@ -0,0 +1,121 @@
+<?php
+/**
+ * @package     Unlimited
+ * @subpackage  Overrider
+ *
+ * @copyright   Copyright (C) 2005 - 2014 Joomlashack. Meritage Assets.  All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// No direct access.
+defined('_JEXEC') or die;
+
+$app = JFactory::getApplication();
+
+require_once(JPATH_THEMES.'/'.$app->getTemplate().'/html/com_content/com_content.helper.php');
+require_once(JPATH_THEMES.'/'.$app->getTemplate().'/wright/html/overrider.php');
+
+if ($this->item->wrightType == 'leading') {
+
+	$classFloat = 'float-' . getIntroImageFloat($this->item);
+
+	if ($classFloat == 'float-right') {
+		$this->item->wrightElementsStructure =
+			Array(
+				"div." . $classFloat,
+					"div.visible-phone",
+						"div.article-image",
+							"image",
+						"/div",
+					"/div",
+
+					"div.article-content",
+						"title",
+						"icons",
+						"article-info",
+						"content",
+					"/div",
+
+					"div.hidden-phone",
+						"div.article-image",
+							"image",
+						"/div",
+					"/div",
+				"/div"
+			);
+	}
+	else{
+
+		$this->item->wrightElementsStructure =
+			Array(
+
+				"div." . $classFloat,
+					"div.article-image",
+						"image",
+					"/div",
+
+					"div.article-content",
+						"title",
+						"icons",
+						"article-info",
+						"content",
+					"/div",
+				"/div"
+			);
+	}
+}
+
+if ($this->item->wrightType == 'intro') {
+
+	$classFloat = 'float-' . getIntroImageFloat($this->item);
+
+	if ($classFloat == 'float-right') {
+		$this->item->wrightElementsStructure =
+			Array(
+				"div." . $classFloat,
+					"div.visible-phone",
+						"div.article-image",
+							"image",
+						"/div",
+					"/div",
+
+					"div.article-content",
+						"title",
+						"icons",
+						"article-info",
+						"content",
+					"/div",
+
+					"div.hidden-phone",
+						"div.article-image",
+							"image",
+						"/div",
+					"/div",
+				"/div"
+			);
+	}
+	else{
+		$this->item->wrightElementsStructure =
+			Array(
+
+				"div." . $classFloat,
+					"div.article-image",
+						"image",
+					"/div",
+
+					"div.article-content",
+						"title",
+						"icons",
+						"article-info",
+						"content",
+					"/div",
+				"/div"
+			);
+	}
+}
+
+$this->item->wrightLegendTop = '';
+$this->item->wrightLegendBottom = '';
+
+include(Overrider::getOverride('com_content.featured','default_item'));
+?>
