5e62f9b3b84135d52415243f9417233f5a9ddcf2
jmarkos <markos@joomlashack.com>
2014-06-25 21:00:12 -0600

    adding horizontal layout for news flash module

---
+++ code/html/mod_articles_news/horizontal.php
@@ -0,0 +1,21 @@
+
+<?php
+// Wright v.3 Override: Joomla 2.5.16 and 3.2
+/**
+ * @package		Joomla.Site
+ * @subpackage	mod_articles_news
+ * @copyright	Copyright (C) 2005 - 2014 Open Source Matters, Inc. All rights reserved.
+ * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// no direct access
+defined('_JEXEC') or die;
+
+	$wrightNewsEnableIcons = false;
+	$wrightTitlePosition = 'above';
+	$wrightEnableLinkContent = true;
+	$wrightMaxColumns = 3;
+
+include(Overrider::getOverride('mod_articles_news','horizontal'));
+?>
+
