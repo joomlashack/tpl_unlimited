7df6ff7913b2880c2aa8220671f16c7b1f912282
jmarkos <markos@joomlashack.com>
2014-07-07 22:56:49 -0600

    fixing style of leading article in blog

--- code/html/com_content/category/blog_item.php
+++ code/html/com_content/category/blog_item.php
@@ -16,53 +16,19 @@ require_once(JPATH_THEMES.'/'.$app->getTemplate().'/html/com_content/com_content
 require_once(JPATH_THEMES.'/'.$app->getTemplate().'/wright/html/overrider.php');
 
 if ($this->item->wrightType == 'leading') {
+	$this->item->wrightElementsStructure =
+		Array(
+			"title",
+			"div.article-image",
+				"image",
+			"/div",
 
-	$classFloat = 'float-' . getIntroImageFloat($this->item);
-
-	if ($classFloat == 'float-right') {
-		$this->item->wrightElementsStructure =
-			Array(
-				"div." . $classFloat,
-					"div.visible-phone",
-						"div.article-image",
-							"image",
-						"/div",
-					"/div",
-
-					"div.article-content",
-						"title",
-						"icons",
-						"article-info",
-						"content",
-					"/div",
-
-					"div.hidden-phone",
-						"div.article-image",
-							"image",
-						"/div",
-					"/div",
-				"/div"
-			);
-	}
-	else{
-
-		$this->item->wrightElementsStructure =
-			Array(
-
-				"div." . $classFloat,
-					"div.article-image",
-						"image",
-					"/div",
-
-					"div.article-content",
-						"title",
-						"icons",
-						"article-info",
-						"content",
-					"/div",
-				"/div"
-			);
-	}
+			"div.article-content",
+				"icons",
+				"article-info",
+				"content",
+			"/div"
+		);
 }
 
 if ($this->item->wrightType == 'intro') {
