039c79c9d5a0460947d26e884c9b27c33f8baecf
jmarkos <markos@joomlashack.com>
2014-07-11 11:07:03 -0600

    TPLUNLIMITED-48 #resolve fixing newsflash ie8 / 9

--- code/js/fallback.js
+++ code/js/fallback.js
@@ -1,25 +1,17 @@
 jQuery(document).ready(function($) {
 
-	function setImages(){
-		jQuery('.main-wrapp > .row > * img').each(function (i) {
-			var newWidth = jQuery('.container-flexbox > div > *').width();
-			jQuery(this).css('max-width' , newWidth + 'px');
-		});
-	}
-
 	function setBlogItems(){
 		jQuery(".items-row .item").equalHeights();
 		jQuery("#grid-top > div > div").equalHeights();
 		jQuery("#grid-bottom2 > div > div").equalHeights();
+		jQuery(".newsflash-horiz > div > div").equalHeights();
 	}
 
 	jQuery(window).load(function(){
-			setImages();
 			setBlogItems();
 	});
 	jQuery(window).resize(function(){
 		if (jQuery(".is_internet.v_9").length) {
-			setImages();
 			setBlogItems();
 		}
 	});
