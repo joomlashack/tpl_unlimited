7fea0f13408e1a2fcc19b01519b667e57d651f6b
jmarkos <markos@joomlashack.com>
2016-01-18 14:02:10 -0600

    upa date

--- code/template.php
+++ code/template.php
@@ -3,7 +3,7 @@
  * @package     Unlimited
  * @subpackage  Template File
  *
- * @copyright   Copyright (C) 2005 - 2015 Joomlashack. Meritage Assets.
+ * @copyright   Copyright (C) 2005 - 2016 Joomlashack. Meritage Assets.
  *              All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  *
