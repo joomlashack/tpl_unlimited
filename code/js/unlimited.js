61eb731df5df4e96d1258740e75eba08a215b719
jmarkos <jkmarcosb@gmail.com>
2017-01-04 10:17:35 -0600

    slider height

--- code/js/unlimited.js
+++ code/js/unlimited.js
@@ -1,6 +1,11 @@
 jQuery(document).ready(function($) {
 
 	var slider_container = jQuery('.jss-slider');
+	var logo_menu_height = jQuery('.wrapp-logo-menu').height();
+	var toolbar_height= jQuery('.wrapp-toolbar').height();
+	var window_height= jQuery(window).height();
+	var top_space = logo_menu_height + toolbar_height;
+	var slider_height =  window_height - top_space ;
 	var slider_id = [];
 
 	slider_container.each(function() {
@@ -8,7 +13,7 @@ jQuery(document).ready(function($) {
 	});
 
 	for (var i = 0; i < slider_id.length; i++) {
-		jQuery('#slider-position').find(slider_id[i] + ' .jss-image').css('height', '100vh');
+		jQuery('#slider-position').find(slider_id[i] + ' .jss-image').height(slider_height);
 	}
 
 });
