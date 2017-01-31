ce8f3a653a0c4c3bfc6c64e9f323876c80b69071
Valentín <htmgarcia@gmail.com>
2017-01-30 19:02:12 -0600

    Fix spacing in footer

--- code/template.php
+++ code/template.php
@@ -142,7 +142,7 @@ defined('_JEXEC') or die('Restricted access');
                 <w:nav containerClass="<?php echo $wrightContainerClass ?>" rowClass="<?php echo $wrightGridMode;?>" wrapClass="navbar-transparent navbar-center" name="bottom-menu" type="menu" />
             <?php endif; ?>
             <div class="<?php echo $wrightContainerClass ?>">
-                <div class="<?php echo $wrightGridMode . ' footer-content p-t-3 p-b-3' ?>">
+                <div class="<?php echo $wrightGridMode . ' footer-content p-t-3' ?>">
                     <?php if ($this->countModules('footer')) : ?>
                         <w:module type="none" name="footer" chrome="wrightflexgrid" />
                     <?php endif; ?>
